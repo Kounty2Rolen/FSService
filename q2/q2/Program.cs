d91b7f4c865db81f24692f9972deef91bb6236e5
Kounty2Rolen <molodcovdaniil123@gmail.com>
2019-05-09 21:09:44 +0300

    Finish Job

--- q2/q2/Program.cs
+++ q2/q2/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Quest 2
+//Kounty_Rolen
+//09.05.2019 Start
+//09.05.2019 Finish
+
+using System;
 
 namespace q2
 {
@@ -6,13 +11,24 @@ namespace q2
     {
         static void Main()
         {
-            Kesha kesha = new Kesha(12, 1);
+            System.Collections.Generic.List<Animal> animals = new System.Collections.Generic.List<Animal>();
+            animals.Add(new Kesha(220,15,"Kesha Like a Boss"));
+            animals.Add(new mycat(5,1,"Kid Anton"));
+            animals[0].info();
+            animals[1].info();
+
+            Kesha kesha = new Kesha(12, 1,"Just Kesha");
             mycat anton = new mycat(25, 2, "anton");
+
+            kesha.info();
             kesha.hpdown(3);
+            kesha.Scream();
+
+            anton.info();
             anton.meow();
             anton.Poop();
             anton.hpdown(25);
-            kesha.Scream();
+
             Console.ReadKey();
 
         }
@@ -35,13 +51,17 @@ namespace q2
             HP = hP;
             Age = age;
         }
+        public virtual void info()
+        {
+            Console.WriteLine($"HP:{HealPoint}\nAge:{Age1}");
 
+        }
         public int HealPoint { get => Age; set => Age = value; }
         public int Age1 { get => Age; set => Age = value; }
     }
     class Cats : Animal
     {
-        private int paws;
+        private int paws=4;
         public Cats(int hP, int age) : base(hP, age)
         {
 
@@ -50,6 +70,11 @@ namespace q2
         {
             Paws -= 1;
 
+        }
+        public override void info()
+        {
+            base.info();
+            Console.WriteLine($"Paws:{Paws}");
         }
         public int Paws { get => paws; set => paws = value; }
 
@@ -61,8 +86,14 @@ namespace q2
         {
             Name = name;
         }
+        public override void info()
+        {
+            Console.WriteLine($"Name:{name}");
+            base.info();
+            Console.WriteLine("\n");
+            
+        }
         private string name;
-
         public string Name { get => name; set => name = value; }
 
         public void Poop()
@@ -76,24 +107,35 @@ namespace q2
         {
 
         }
-        private bool egg;
-
+        private bool egg=false;
+        public override void info()
+        {
+            base.info();
+            Console.WriteLine($"Egg?:{Egg}");
+        }
         public bool Egg { get => egg; set => egg = value; }
     }
     class Kesha : Birds
     {
-        public Kesha(int hp, int age) : base(hp, age)
+        public Kesha(int hp, int age, string name) : base(hp, age)
         {
-
+            Name = name;
         }
+        private string name;
+        public string Name { get => name; set => name = value; }
         private bool injail;
 
         public bool Injail { get => injail; set => injail = value; }
-
+        public override void info()
+        {
+            Console.WriteLine($"Name:{Name}");
+            base.info();
+            Console.WriteLine($"Injail?:{Injail}\n");
+        }
         public void Scream()
         {
 
-            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAA!");
+            Console.WriteLine($"{Name} Scream: AAAAAAAAAAAAAAAAAAAAAA!");
         }
     }
 }
