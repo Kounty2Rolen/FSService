89529db7061ea36b61b8aab02561342b3bb110d6
Kounty2Rolen <molodcovdaniil123@gmail.com>
2019-05-13 18:47:03 +0300

    Add models dir & split clases into each file

---
+++ q2/q2/Models/AnimalClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace q2
+{
+    public class Animal
+    {
+        private int HP;
+        private int Age;
+        public void hpdown(int i)
+        {
+            HP -= i;
+            if (HP <= 0)
+            {
+                Console.WriteLine("Him Die NOOOOOO! \n God pls NO!!!!");
+            }
+        }
+
+        public Animal(int hP, int age)
+        {
+            HP = hP;
+            Age = age;
+        }
+        public virtual string info()
+        {
+            return $"HP:{HealPoint}\nAge:{Age1}";
+
+        }
+        public int HealPoint { get => Age; set => Age = value; }
+        public int Age1 { get => Age; set => Age = value; }
+    }
+}
---
+++ q2/q2/Models/BirdsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace q2.Models
+{
+    class Birds : Animal
+    {
+        public Birds(int hp, int age) : base(hp, age)
+        {
+
+        }
+        private bool egg = false;
+        public override string info()
+        {
+            return base.info() + $"\nEgg?:{Egg}\n";
+        }
+        public bool Egg { get => egg; set => egg = value; }
+    }
+}
